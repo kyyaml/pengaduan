40f296e9af595f1fd0150d3ea229e5705b89f945
kyyaml <amaliarizky1987@gmail.com>
2023-11-03 07:55:18 +0700

    pengaduan

---
+++ app/Models/Tanggapan.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tanggapan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tanggapan';
+
+    protected $primaryKey = 'id_tanggapan';
+
+    protected $fillable = [
+        'tgl_tanggapan',
+        'id_pengaduan',
+        'tanggapan',
+        'id_petugas',
+
+    ];
+}
